f2ce141d0f9a03b68cdb60bdf83f160de08fc147
MichaelJames16 <m.harrelson86@gmail.com>
2016-10-17 20:30:03 -0600

    Main Menu
    Added a main menu that loads a test level when clicking either test or
    play.

---
+++ Lazarus/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Test()
+    {
+        SceneManager.LoadScene("TestLevel");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
